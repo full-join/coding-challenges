d781c49c61564e1960a1aa35bdd7a57c858add03
Tiffany Tipton <tiffany.tipton520@gmail.com>
2021-04-18 11:17:08 -0500

    refactored click handler

--- memory-game/public/javascripts/game.js
+++ memory-game/public/javascripts/game.js
@@ -19,18 +19,18 @@ window.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".square").forEach((item) => {
     // console.log(item)
     cards.push(new Card(item));
-
-    item.addEventListener("click", (e) => {
-      // handle the click event
-      let square = e.target.parentElement.parentElement;
-      let squareNumber = square.id;
-      let card = cards[squareNumber];
-      if (!card.flipped) {
-        card.flip();
-      } else {
-        card.unflip();
-      }
-      console.log(`Square ${squareNumber} clicked`);
-    });
+    item.addEventListener("click", onSquareClick);
   });
 });
+
+function onSquareClick(e) {
+  let square = e.target.parentElement.parentElement;
+  let squareNumber = square.id;
+  let card = cards[squareNumber];
+  if (!card.flipped) {
+    card.flip();
+  } else {
+    card.unflip();
+  }
+  console.log(`Square ${squareNumber} clicked`);
+}
