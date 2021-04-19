c51f1ac0a35b37d5a0600f131b3a2e8b5f97a84f
Julie Nisbet <jmnisbet@gmail.com>
2021-04-19 14:11:57 -0700

    adds card dom element and reset method

--- memory-game/public/javascripts/classes/card.js
+++ memory-game/public/javascripts/classes/card.js
@@ -1,12 +1,51 @@
 export default class Card {
-    constructor(value, domElem){
+    constructor(value, id){
         this.value = value
-        // TODO: generate this DOM element automatically
-        this.domElem = domElem
+        this.domElement = this.createDomElement(id)
         this.flipped = false
     }
+
+    static isMatch(cards){
+        // TODO: update to not care about length of array
+        return cards[0].value === cards[1].value
+    }
+
     flip(){
-        this.flipped = !this.flipped
-        this.domElem.classList.toggle("flipped")
+        if(this.flipped) return;
+        this.flipped = true;
+        this.domElement.classList.add("flipped")
+    }
+
+    reset(){
+        if(!this.flipped) return;
+        this.flipped = false;
+        this.domElement.classList.remove("flipped")
+    }
+
+    // div.flip-card(id=n++)
+    //       div.flip-card-inner
+    //         div.flip-card-front
+    //         div.flip-card-back.bg-yellow-400.text-5xl
+    createDomElement(id){
+        const div1 = document.createElement('div')
+        div1.classList.add("flip-card")
+        div1.id = id
+        const div2 = document.createElement('div')
+        div2.classList.add("flip-card-inner")
+        const div3a = document.createElement('div')
+        div3a.classList.add("flip-card-front")
+        const div3b = document.createElement('div')
+        div3b.classList.add("flip-card-back")
+        div3b.classList.add("bg-yellow-400")
+        div3b.classList.add("text-5xl")
+        div1.appendChild(div2)
+        div2.appendChild(div3a)
+        div2.appendChild(div3b)
+
+        div3b.innerText = this.value
+        
+        div1.addEventListener("click", this.flip.bind(this))
+
+        return div1
     }
 }
--- memory-game/public/javascripts/tests/card-test.js
+++ memory-game/public/javascripts/tests/card-test.js
@@ -2,20 +2,69 @@ import Card  from '../classes/card.js';
 const test = QUnit.test;
 
 
-test('it should have a value and a flipped boolean', expect => {
-    const card = new Card(4)
 
+test('it should have a value, a flipped boolean and a DOM element', expect => {
+    const card = new Card(4, 2)
+    const expectedDOM = '<div class="flip-card" id="2"><div class="flip-card-inner"><div class="flip-card-front"></div><div class="flip-card-back bg-yellow-400 text-5xl">4</div></div></div>'
     expect.equal(card.value, 4)
     expect.equal(card.flipped, false)
+    expect.equal(card.domElement.outerHTML, expectedDOM)
 
 })
 
-test('#flip should toggle flipped value', expect => {
-    const elem = document.createElement("div")
-    const card = new Card(4, elem)
+test('Card#isMatch should return true if cards are a match', expect => {
+    const card1 = new Card(4)
+    const card2 = new Card(4)
+
+    expect.equal(Card.isMatch([card1, card2]), true)
+})
+
+test('Card#isMatch should return false if cards are not a match', expect => {
+    const card1 = new Card(4)
+    const card2 = new Card(5)
+
+    expect.equal(Card.isMatch([card1, card2]), false)
+})
+test('#flip should set flipped to true', expect => {
+    const card = new Card(4)
     
     card.flip()
 
     expect.equal(card.flipped, true)
-    expect.equal(elem.classList.contains("flipped"), true)
+    expect.equal(card.domElement.classList.contains("flipped"), true)
 })
+
+test('#flip should return if card already flipped', expect => {
+    const card = new Card(4)
+    card.domElement.classList.add("flipped")
+    card.flipped = true
+    
+    card.flip()
+
+    expect.equal(card.flipped, true)
+    expect.equal(card.domElement.classList.contains("flipped"), true)
+})
+
+test('#reset should set flipped to false', expect => {
+    const card = new Card(4)
+    card.flipped = true
+    card.domElement.classList.add("flipped")
+    
+    card.reset()
+
+    expect.equal(card.flipped, false)
+    expect.equal(card.domElement.classList.contains("flipped"), false)
+})
+
+test('#reset should return if card already reset', expect => {
+    const card = new Card(4)
+    
+    card.reset()
+
+    expect.equal(card.flipped, false)
+    expect.equal(card.domElement.classList.contains("flipped"), false)
+})
+
+
+
+// test()
