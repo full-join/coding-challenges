a5bbc06bfe549961a74240af80ce087ecefbec44
Julie Nisbet <jmnisbet@gmail.com>
2021-04-19 14:13:33 -0700

    remove old tests / functions

--- memory-game/public/javascripts/game-helpers/reset.js
+++
@@ -1,9 +0,0 @@
-export const reset = flipped => {
-    return new Promise(resolve => {
-        setTimeout(()=>{
-            flipped.map(c => c.classList.toggle("flipped"))
-            flipped.length = 0
-            resolve()
-        }, 1000)
-    })
-}
